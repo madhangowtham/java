29c5ce3c854f8ba6243d0161eb385cfdc69127c5
madhangowtham <47312436+madhangowtham@users.noreply.github.com>
2019-02-12 11:55:49 +0530

    Create Largest2Num.java

---
+++ Largest2Num.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Largest2Num{
+public static void main(String args[]){
+Scanner scan = new Scanner(System.in);
+System.out.print("Enter the first number : ");
+float num1=scan.nextInt();		
+System.out.print("Enter the Second number : ");
+float num2=scan.nextInt();	
+
+/** function is an inbuilt function in Java which returns maximum of two numbers.
+ The arguments are taken in int, double, float and long**/
+
+System.out.println("The large num of the three numbers is " +Math.max(num1,num2));
+System.out.println("The small num of the three numbers is " +Math.min(num1,num2));
+}
+}
