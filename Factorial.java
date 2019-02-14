435fa9fa8bd30d4182c6a0eb3d6d32c88319a1a8
madhangowtham <47312436+madhangowtham@users.noreply.github.com>
2019-02-14 09:10:38 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -5,9 +5,8 @@ Scanner scan = new Scanner(System.in);
 System.out.print("Enter the number :");
 int num=scan.nextInt();
 int factorial= 1;
-for(int i = 1; i <= num; ++i){
+for(int i = 1; i <= num; ++i)
 factorial =factorial*i;
-}
 System.out.print("Factorial is :"+factorial);
 }
 }
